088ebbd3d203b874a76c8c203b0647546a5797d6
Byelmanov <yelmanovbohdan@gmail.com>
2019-06-14 01:24:55 +0300

    add burger

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-// button on which user clicks -> scroll to the top of the page
+$(function() {
+	$( ".cross" ).hide();
+	$( ".menu" ).hide();
+	$( ".hamburger" ).click(function() {
+		$( ".menu" ).slideToggle( "slow", function() {
+			$( ".hamburger" ).hide();
+			$( ".cross" ).show();
+		});
+	});
+	$( ".cross" ).click(function() {
+		$( ".menu" ).slideToggle( "slow", function() {
+			$( ".cross" ).hide();
+			$( ".hamburger" ).show();
+		});
+	});
+});
+
+
+// button to the top 
+
 $(".button").on("click", function(){
-	$("body, html").animate({scrollTop: 0}, 500);
+    $("body, html").animate({scrollTop: 0}, 500);
 });
